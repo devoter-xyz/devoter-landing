cca8a3361c46cfde7c9af6e74cdb0dd96f80318a
Kenneth Nelson <kenneth.nelson.dev@gmail.com>
2025-07-17 01:06:07 -0700

    set global font as kumbh sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Kumbh_Sans } from "next/font/google";
 import "./globals.css";
-import ParticleSystem from "../components/ParticleSystem";
-import Navbar from "@/components/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const kumbhSans = Kumbh_Sans({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-kumbh-sans",
 });
 
 export const metadata: Metadata = {
@@ -26,13 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ParticleSystem />
-        <Navbar />
-        {children}
-      </body>
+      <body className={`${kumbhSans.variable} antialiased`}>{children}</body>
     </html>
   );
 }
