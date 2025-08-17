3cbd81306f064a08c465b6799d44404563bcf204
Jack Ulf <metanodedreamer@gmail.com>
2025-08-17 09:11:46 +0000

    removed format date

--- src/components/changelog/ChangelogContent.tsx
+++ src/components/changelog/ChangelogContent.tsx
@@ -41,13 +41,6 @@ const getChangeTypeColor = (type: string) => {
   }
 };
 
-const formatDate = (dateString: string) => {
-  return new Date(dateString).toLocaleDateString("en-US", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
-};
 
 export default function ChangelogContent() {
   return (
